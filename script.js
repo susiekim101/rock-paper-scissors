b10cbc933ee36f134f8a1c8c57a3e9457d4b8313
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-04-07 09:43:01 -0700

    Added JSX logic to update score and results

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice) {
-    function getComputerChoice() {
-        const computer = Math.floor((Math.random() * 3));
-        if (computer == 0) {
-            return "rock";
-        }
-        else if (computer == 1) {
-            return "paper";
-        }
-        else {
-            return "scissors";
-        }
-    }
-    let computerChoice = getComputerChoice();
+const playerContainer = document.querySelector(".playerContainer");
+const playerScore = document.createElement("h2");
+playerScore.setAttribute("id", "playerScore");
+playerScore.textContent = humanScore;
+playerContainer.appendChild(playerScore);
 
+const computerContainer = document.querySelector(".computerContainer");
+const compScore = document.createElement("h2");
+compScore.setAttribute("id", "computerScore");
+compScore.textContent = computerScore;
+computerContainer.appendChild(compScore);
+
+const computerOption = document.querySelector("#computerButton");
+const computerIcon = document.createElement("p");
+computerIcon.setAttribute("id", "computerIcon");
+computerIcon.textContent = "❓";
+computerOption.append(computerIcon);
+
+const conclusionContainer = document.querySelector(".conclusionContainer");
+const winnerText = document.createElement("p");
+winnerText.setAttribute("id", "winner");
+winnerText.textContent = "Playing game..."
+conclusionContainer.append(winnerText)
+
+function getComputerChoice() {
+    const computer = Math.floor((Math.random() * 3));
+    if (computer == 0) {
+        computerIcon.textContent = "👊"
+        return "rock";
+    }
+    else if (computer == 1) {
+        computerIcon.textContent = "🖐️"
+        return "paper";
+    }
+    else {
+        computerIcon.textContent = "✌️"
+        return "scissors";
+    }
+}
+
+function calcResult(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
         return "Tie!";
     }
@@ -23,13 +49,36 @@ function playRound(humanChoice) {
         (humanChoice == "paper" && computerChoice == "scissors") ||
         (humanChoice == "scissors" && computerChoice == "rock")) {
         computerScore++;
+        compScore.textContent = computerScore
         return "Computer won!";
     }
     else {
         humanScore++;
+        playerScore.textContent = humanScore
         return "You won!";
     }
 }
+
+function updateResult() {
+    if (humanScore >= 5) {
+        winnerText.textContent = "Player won the game!"
+        const buttons = document.querySelectorAll("button");
+        buttons.forEach( (button) => button.disabled = true);
+    }
+    else if (computerScore >= 5) {
+        winnerText.textContent = "Computer won the game!"
+        const buttons = document.querySelectorAll("button");
+        buttons.forEach( (button) => button.disabled = true);
+    }
+}
+
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    calcResult(humanChoice, computerChoice);
+    updateResult();
+}
+
+
 
 const rockButton = document.querySelector("#rockButton")
 rockButton.addEventListener("click", () => console.log(playRound("rock")))
@@ -39,31 +88,3 @@ paperButton.addEventListener("click", () => console.log(playRound("paper")))
 
 const scissorsButton = document.querySelector("#scissorsButton")
 scissorsButton.addEventListener("click", () => console.log(playRound("scissors")))
-
-/*
-function getHumanChoice() {
-    const human = prompt("Options: rock, paper, scissors").toLowerCase();
-    return human;
-}
-
-function playGame() {
-
-    for (let i = 0; i < 5; i++) {
-        human = getHumanChoice();
-        computer = getComputerChoice();
-        console.log("Computer: " + computer);
-        playRound(human, computer);
-        console.log("User score: " + humanScore + " | Computer score: " + computerScore);
-    }
-
-    if (humanScore > computerScore) {
-        console.log("Congrats! You won");
-    }
-    else if (computerScore > humanScore) {
-        console.log("Sorry, the computer won.");
-    }
-    else {
-        console.log("It's a tie!");
-    }
-}
-playGame(); */
